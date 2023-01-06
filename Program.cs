7615434717e958f879b83370b3f50bf79ef8e7f2
Your Name <you@example.com>
2023-01-06 18:02:52 +0400

    заведение массива

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void FillArr (string[] array)
+{   for (int i=0; i<array.Length; i++){
+        array[i] = Console.ReadLine();}
+}   
+
+Console.WriteLine("введите число элементов массива");
+int len1 = int.Parse(Console.ReadLine());
+string [] Array = new string [len1];
+Console.WriteLine("Введите массив столбцом");
+FillArr(Array);
