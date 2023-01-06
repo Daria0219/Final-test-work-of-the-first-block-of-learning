39bf6c5711cb356b5431e9a7a78407ab68c1406a
Your Name <you@example.com>
2023-01-06 18:55:06 +0400

    решение

--- Program.cs
+++ Program.cs
@@ -1,10 +1,36 @@
-﻿void FillArr (string[] array)
-{   for (int i=0; i<array.Length; i++){
+﻿void FillArr (string [] array)
+{   for (int i=0; i < array.Length; i++){
         array[i] = Console.ReadLine();}
 }   
+
+void PrintArray (string [] arr)
+{   Console.Write(arr[0]);
+    int pos=1;
+    while (pos < arr.Length)
+        {Console.Write($", {arr[pos]}");
+        pos++;}
+    }
 
 Console.WriteLine("введите число элементов массива");
 int len1 = int.Parse(Console.ReadLine());
 string [] Array = new string [len1];
 Console.WriteLine("Введите массив столбцом");
+
 FillArr(Array);
+Console.Write("Исходный массив: ");
+PrintArray(Array);
+
+void ChangeMassive (string[]arr){
+List<string> newlist = new List<string>();
+for (int i=0; i < arr.Length; i++){
+    if (arr[i].Length<=3) {
+        newlist.Add(arr[i]);
+        }
+    }
+string [] newArr = newlist.ToArray();
+PrintArray(newArr);
+}
+
+Console.WriteLine();
+Console.Write("Преобразованный массив: ");
+ChangeMassive(Array);
