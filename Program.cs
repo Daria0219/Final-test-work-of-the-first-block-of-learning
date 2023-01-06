2616a9fc2e44b7857840d0d0684bee8b681391fe
Your Name <you@example.com>
2023-01-06 19:33:14 +0400

    решение другим способом

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿void FillArr (string [] array)
+﻿/* void FillArr (string [] array)
 {   for (int i=0; i < array.Length; i++){
         array[i] = Console.ReadLine();}
 }   
@@ -34,3 +34,48 @@ PrintArray(newArr);
 Console.WriteLine();
 Console.Write("Преобразованный массив: ");
 ChangeMassive(Array);
+*/
+
+// Второй способ решения
+
+void FillArr (string [] array)
+{   for (int i=0; i < array.Length; i++){
+        array[i] = Console.ReadLine();}
+}   
+
+void PrintArray (string [] arr)
+{   Console.Write(arr[0]);
+    int pos=1;
+    while (pos < arr.Length)
+        {Console.Write($", {arr[pos]}");
+        pos++;}
+    }
+
+Console.WriteLine("введите число элементов массива");
+int len1 = int.Parse(Console.ReadLine());
+string [] Array = new string [len1];
+Console.WriteLine("Введите массив столбцом");
+
+FillArr(Array);
+Console.Write("Исходный массив: ");
+PrintArray(Array);
+
+void NewMassive (string[]arr){
+int len2 = 0;
+for (int i=0; i < arr.Length; i++){
+    if (arr[i].Length<=3) {
+        len2++;}
+    }
+string [] newArr = new string [len2];
+int k=0;
+for (int i=0; i < arr.Length; i++){
+    if (arr[i].Length<=3) {
+        newArr[k]=arr[i];
+        k++;
+    }}
+PrintArray(newArr);
+}
+
+Console.WriteLine();
+Console.Write("Преобразованный массив: ");
+NewMassive(Array);
